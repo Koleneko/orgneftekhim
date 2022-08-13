fa9968dd177d35324dede00f384af2a9c3b23b75
Koleneko <vkoleneko@gmail.com>
2022-08-14 00:15:17 +0500

    added pages for all categories

---
+++ src/pages/certificates.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Certificates: NextPage = () => {
+  return <div className={"placeholder-box"}>Паспорта качества</div>;
+};
+
+export default Certificates;
