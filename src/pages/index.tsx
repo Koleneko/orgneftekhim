884ba61bedfcf0e3c38ae4ae04c0be3255f99503
vkoleneko <vkoleneko@gmail.com>
2022-09-16 16:18:32 +0500

    redirect to /about

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
@@ -8,15 +8,18 @@ const Home: NextPage = () => {
         <title>ОРГНЕФТЕХИМ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={
-          "mobile:text-[20px] mobile:h-48 mobile:w-72  text-4xl flex items-center justify-center bg-[#ECECEC] rounded-2xl tablet:h-[20%] tablet:w-[40%]"
-        }
-      >
-        САЙТ В РАЗРАБОТКЕ
-      </div>
+      <div className={"placeholder-box"}>САЙТ В РАЗРАБОТКЕ</div>
     </>
   );
 };
 
 export default Home;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    redirect: {
+      destination: "/about",
+    },
+    props: {},
+  };
+};
