8d5bda2be5215961b497e271c036ff4795a633cc
vkoleneko <vkoleneko@gmail.com>
2022-09-20 20:54:43 +0500

    changed footer phone number

--- src/components/core/footer/index.tsx
+++ src/components/core/footer/index.tsx
@@ -39,8 +39,8 @@ const Footer: FC = () => {
             142403, Россия, Московская область, г. Ногинск, ул. Индустриальная,
             д. 41В, Э/О 2/332.
             <br />
-            Телефон: +7(915) 097-39-27, +7(915) 097-23-97, E-mail:
-            info@orgneftekhim.ru info@orgneftekhim.ru
+            Телефон: 8 (800) 550-7-550, E-mail: info@orgneftekhim.ru
+            info@orgneftekhim.ru
           </div>
         </div>
       </div>
